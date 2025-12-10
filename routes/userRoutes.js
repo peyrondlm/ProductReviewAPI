422e73471466f6066a2e5f203111e4893e85b6f2
Pedro Lopez <peyron.dlm@gmail.com>
2025-12-10 12:12:57 -0600

    Corrigiendo la autorizacion del token en usuarios

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,12 +6,14 @@ import {
   update,
   remove
 } from "../controllers/userController.js";
+import { authenticateToken } from "../middlewares/auth.js";
+
 const router = express.Router();
 
 router.get("/", getAll);
 router.get("/:id", getById);
 router.post("/", create);
-router.put("/:id", update);
-router.delete("/:id", remove);
+router.put("/:id", authenticateToken, update);
+router.delete("/:id", authenticateToken, remove);
 
 export default router;
